b42817daa9f6694558e6d10160277eed5cefe5d7
mandeepbhutani <bhutanimandeep@gmail.com>
2015-01-28 17:56:35 -0600

    Added comments to Problem 1

--- ProjectEulerProblem1.py
+++ ProjectEulerProblem1.py
@@ -1,15 +1,17 @@
 """
-Project Euler - Problem 1: Multiples of 3 and 5
+Project: Project Euler - Problem 1: Multiples of 3 and 5
+Author: Mandeep Bhutani
+Date: 1/28/2015
 
+Problem:
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
-
 """
 def prime_number():
 	result = 0
 	for i in range(1,1000):
-    		if i % 3 == 0 or i % 5 == 0:
-       			result += i
+    		if i % 3 == 0 or i % 5 == 0: # Find number that are divisible by 3 or 5
+       			result += i # Add these numbers to result
     	return result
 print prime_number()
