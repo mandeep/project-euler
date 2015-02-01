b87796b0d03952a43ed1c47c759a1f5fa7f5f1a5
mandeepbhutani <bhutanimandeep@gmail.com>
2015-02-01 11:52:20 -0600

    Sum Square Difference

--- ProjectEulerProblem6.py
+++ ProjectEulerProblem6.py
@@ -1,5 +1,5 @@
 """
-Project: Project Euler - Problem 6: Sum square difference
+Project: Project Euler - Problem 6: Sum Square Difference
 Author: Mandeep Bhutani
 Date: 1/30/2015
 
@@ -13,17 +13,5 @@ The square of the sum of the first ten natural numbers is,
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
-def first(): # Function computes the sum of the squares
-    total = 0
-    for y in range(101):
-        total += y ** 2 # For each number in range, calculate its square and add to total
-    return total
-
-def second(): # Function computes the square of the sum
-    result = 0
-    for x in range(101):
-        result += x # Sum each number in range then add to result
-        resultant = result ** 2 # Square the sum of the numbers found in the range
-    return resultant
-    
-print second() - first() 
+print sum([x for x in range(101)])**2 - sum([x ** 2 for x in range(101)])
+# Using the sum() function as well as list comprehension to enhance readability.
