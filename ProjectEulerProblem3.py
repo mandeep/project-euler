61af1ddbc91417231b1812e16fedc85c6bbd1c9c
mandeepbhutani <bhutanimandeep@gmail.com>
2015-02-01 13:14:38 -0600

    Largest Prime Factor

--- ProjectEulerProblem3.py
+++ ProjectEulerProblem3.py
@@ -1,8 +1,9 @@
 """
 Title: Project Euler - Problem 3: Largest Prime Factor
+Author: Mandeep Bhutani
 Date: 2/1/2015
 
-Problem:  The prime factors of 13195 are 5, 7, 13 and 29.
+Problem: The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 n = 600851475143 
