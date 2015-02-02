0ddce95e9067597c4dae9b923537db5330a96374
mandeepbhutani <bhutanimandeep@gmail.com>
2015-02-01 21:51:33 -0600

    Factorial Digit Sum

--- ProjectEulerProblem20.py
+++ ProjectEulerProblem20.py
@@ -4,7 +4,7 @@ Author: Mandeep Bhutani
 Date: 2/1/2015
 
 Problem:
-n! means n * n(n-1) * ... * 3 * 2 * 1
+n! means n * (n-1) * ... * 3 * 2 * 1
 For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!
