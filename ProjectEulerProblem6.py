3d163f29ec3fb782cff77a5269ac316051209bb3
mandeepbhutani <bhutanimandeep@gmail.com>
2015-02-15 10:57:56 -0600

    Sum Square Difference

--- ProjectEulerProblem6.py
+++ ProjectEulerProblem6.py
@@ -15,6 +15,20 @@ numbers and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred
 natural numbers and the square of the sum.
 """
-print sum([x for x in range(101)])**2 - sum([x ** 2 for x in range(101)])
-# Using the sum() function as well as list
-# comprehension to enhance readability.
+
+
+def sum_squares():
+    result = 0
+    for x in range(1, 101):
+        result += x ** 2
+    return result
+
+
+def square_sums():
+    total = 0
+    for x in range(1, 101):
+        total += x
+    total **= 2
+    return total
+
+print square_sums() - sum_squares()
