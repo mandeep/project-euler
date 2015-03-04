40aa0905684bbb2e6839c91624ab212e434efdaf
mandeepbhutani <bhutanimandeep@gmail.com>
2015-03-03 20:22:06 -0600

    Summation of Primes

--- Problem 10 - Summation of Primes.py
+++ Problem 10 - Summation of Primes.py
@@ -9,17 +9,23 @@ Find the sum of all the primes below two million.
 References: http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes;
 https://docs.python.org/2/library/stdtypes.html
 """
+from math import sqrt
 
 
-def eratosthenes(n):
-    total = set()  # Used to generate unique multiples
+def is_prime(n):
+    for x in range(2, int(sqrt(n) + 1)):
+        if n % x == 0:
+            return False
+    else:
+        return True
+
+
+def summation(s):
     result = []
-    for p in range(2, n+1):  # The range extends to n+1 in order to include
-        # n in the calculation
-        if p not in total:
-            result.append(p)  # Add all primes to result
-            total.update(range(p*p, n+1, p))  # Enumerating multiples
-            # by counting to n in increments of p
-    return result
-
-print sum(eratosthenes(2000000))
+    p = 1
+    while p < s:
+        p += 1
+        if is_prime(p) == True:
+            result.append(p)
+    return sum(result)
+print summation(2000000)
