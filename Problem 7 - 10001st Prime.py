1d771b1a38a42c472caab4168318da85c0c195d0
mandeepbhutani <bhutanimandeep@gmail.com>
2015-03-07 19:39:47 -0600

    10,001st Prime

--- Problem 7 - 10001st Prime.py
+++ Problem 7 - 10001st Prime.py
@@ -4,7 +4,7 @@ Author: Mandeep Bhutani
 Date: 3/3/2015
 
 Problem: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
-we can see that the 6th prime is 13. What is the 10 001st prime number?
+we can see that the 6th prime is 13. What is the 10,001st prime number?
 """
 from math import sqrt
 
@@ -17,11 +17,11 @@ def is_prime(n):
         return True
 
 
-def term(s):
+def term(t):
     result = []
     p = 1
     counter = 0
-    while counter < s:
+    while counter < t:
         p += 1
         if is_prime(p) == True:
             result.append(p)
