1e3f4c57fd46b403c1df11a2bab680e59f124db3
mandeepbhutani <bhutanimandeep@gmail.com>
2015-03-07 19:39:24 -0600

    Largest Prime Factor

--- Problem 3 - Largest Prime Factor.py
+++ Problem 3 - Largest Prime Factor.py
@@ -6,15 +6,19 @@ Date: 2/1/2015
 Problem: The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
-n = 600851475143
-i = 2  # 2 is the smallest prime number, thus it is the first iterable.
 
-# If n is not prime, there must be atleast one divisor less than the root of n.
-# Another way to write this is the square of i (a factor) must be less than n.
-while i ** 2 < n:
-    while n % i == 0:  # If n divided by i leaves no remainder, i is a factor.
-        n /= i  # The largest prime factor unknown. Once the while loop fails,
-        # the value of n is changed to n divided by its largest factor
-        # resulting in the largest prime factor.
-    i += 1  # Increases i until the square of i is greater than or equal to n.
-print n
+
+def largest_prime_factor(n):
+    i = 2
+
+    # If n is not prime, atleast one divisor less than the root of n exists.
+    # Another way to write this is the square of i must be less than n.
+    while i * i < n:
+
+        # Divide n by each of its prime factors until it equals its largest
+        # prime factor
+        while n % i == 0:
+            n /= i
+        i += 1
+    return n
+
