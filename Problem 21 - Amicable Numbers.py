16ab35669a3ccc4f4ad4e059042256859a20f048
mandeepbhutani <bhutanimandeep@gmail.com>
2015-03-12 20:27:40 -0500

    Amicable Numbers

---
+++ Problem 21 - Amicable Numbers.py
@@ -0,0 +1,28 @@
+"""
+Title: Project Euler - Problem 21: Amicable Numbers
+Author: Mandeep Bhutani
+Date: 3/11/2015
+
+Problem: Let d(n) be defined as the sum of proper divisors of n (numbers less
+than n which divide evenly into n). If d(a) = b and d(b) = a, where a != b,
+then a and b are an amicable pair and each of a and b are called amicable
+numbers. For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20,
+22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are
+1, 2, 4, 71 and 142; so d(284) = 220. Evaluate the sum of all the amicable
+numbers under 10,000.
+"""
+result = []
+for numbers in range(1, 10001):
+    first, second = [], []
+    for i in range(1, numbers):
+        if numbers % i == 0:
+            first.append(i)
+    first_sum = sum(first)
+    for j in range(1, numbers):
+        if first_sum % j == 0:
+            second.append(j)
+    second_sum = sum(second)
+    if second_sum == numbers and (first_sum - second_sum) > 2:
+        result.append(first_sum)
+        result.append(second_sum)
+print sum(result)
