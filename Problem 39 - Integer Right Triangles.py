09ffa242ed4bf3094877d1945f9ca88d88db93d0
Mandeep Bhutani <bhutanimandeep@gmail.com>
2015-03-29 18:02:38 -0700

    Integer Right Triangles

---
+++ Problem 39 - Integer Right Triangles.py
@@ -0,0 +1,23 @@
+"""
+Project: Project Euler - Problem 39: Integers Right Triangles
+Author: Mandeep Bhutani
+Date: 3/29/2015
+
+Problem: If p is the perimeter of a right angle triangle with integral length
+sides, {a, b, c}, there are exactly three solutions for p = 120.
+{20,48,52}, {24,45,51}, {30,40,50}
+For which value of p <= 1000, is the number of solutions maximised?
+"""
+
+result = []
+for x in range(1, 1001):
+    total = []
+    for a in range(1, x + 1):
+        for b in range(1, x + 1):
+            c = x - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                if sorted((a, b, c)) not in total:
+                    total.append(sorted((a, b, c)))
+    if len(total) > 3:
+        result.append((len(total), x))
+print max(result)
