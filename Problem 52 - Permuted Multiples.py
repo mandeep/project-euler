8623e958dec82fefa53149ce770cde642a957bfa
Mandeep Bhutani <bhutanimandeep@gmail.com>
2015-04-05 19:43:05 -0500

    Permuted Multiples

--- Problem 52 - Permuted Multiples.py
+++ Problem 52 - Permuted Multiples.py
@@ -8,10 +8,8 @@ contain exactly the same digits, but in a different order. Find the smallest
 positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
 """
 x = 125874
-while True:
-    x += 1
-    if sorted(set(str(x))) == sorted(set(str(x * 2))) == \
-        sorted(set(str(x * 3))) == sorted(set(str(x * 4))) == \
-            sorted(set(str(x * 5))) == sorted(set(str(x * 6))):
-        print x
-        break
+ans = set(str(x))
+for y in xrange(1, 7):
+    while ans != set(str(x * y)):
+        x += 1
+print x
