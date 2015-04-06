e455ef0e1f57fd28f107887a94593094318f2ddf
Mandeep Bhutani <bhutanimandeep@gmail.com>
2015-04-05 23:18:34 -0500

    Permuted Multiples

--- Problem 52 - Permuted Multiples.py
+++ Problem 52 - Permuted Multiples.py
@@ -7,9 +7,8 @@ Problem: It can be seen that the number, 125874, and its double, 251748,
 contain exactly the same digits, but in a different order. Find the smallest
 positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
 """
-x = 125874
-ans = set(str(x))
-for y in xrange(1, 7):
-    while ans != set(str(x * y)):
+x = 1
+for y in xrange(2, 7):
+    while sorted(str(x)) != sorted(str(x * y)):
         x += 1
 print x
