e809c40d33dcd91d126fe69dd4ea6c70d169e658
Mandeep Bhutani <bhutanimandeep@gmail.com>
2015-04-12 14:12:21 -0500

    Multiples of 3 and 5

--- Problem 1 - Multiples of 3 and 5.py
+++ Problem 1 - Multiples of 3 and 5.py
@@ -15,5 +15,5 @@ def multiples():
     for i in range(1, 1000):
         if i % 3 == 0 or i % 5 == 0:  # Find factors of 3 or 5
             result += i  # Add these numbers to result
-        return result
+    return result
 print multiples()
