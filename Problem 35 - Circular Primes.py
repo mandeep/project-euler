eac57469515f4e5b3795f68b852b8374ea4c26b3
Mandeep Bhutani <bhutanimandeep@gmail.com>
2015-04-18 10:49:49 -0500

    Circular Primes

--- Problem 35 - Circular Primes.py
+++ Problem 35 - Circular Primes.py
@@ -22,8 +22,8 @@ def is_prime(n):
 
 def is_circular_prime(m):
     m = str(m)
-    primes = [m[dex:] + m[:dex] for dex, number in enumerate(m)]
-    return all([is_prime(int(k)) for k in primes]) == True
+    total = [m[dex:] + m[:dex] for dex, number in enumerate(m)]
+    return all([is_prime(int(k)) for k in total]) == True
 
 
 count = 13
