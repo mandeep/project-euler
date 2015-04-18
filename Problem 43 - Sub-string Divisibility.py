52a12ee39e1b1702aee7a63fc346b3ea105362de
Mandeep Bhutani <bhutanimandeep@gmail.com>
2015-04-18 10:53:01 -0500

    Sub-string Divisibility

--- Problem 43 - Sub-string Divisibility.py
+++ Problem 43 - Sub-string Divisibility.py
@@ -3,13 +3,12 @@ Project: Project Euler - Problem 43: Sub-string Divisibility
 Author: Mandeep Bhutani
 Date: 4/18/2015
 
-Problem: A permutation is an ordered arrangement of objects. For example, 3124
-is one possible permutation of the digits 1, 2, 3 and 4. If all of the
-permutations are listed numerically or alphabetically, we call it lexicographic
-order. The lexicographic permutations of 0, 1 and 2 are:
-012, 021, 102, 120, 201, 210. What is the millionth lexicographic permutation
-of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
-
+Problem: The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the
+digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+d2d3d4=406 is divisible by 2, d3d4d5=063 is divisible by 3, d4d5d6=635 is divisible by 5,
+d5d6d7=357 is divisible by 7, d6d7d8=572 is divisible by 11, d7d8d9=728 is divisible by 13,
+d8d9d10=289 is divisible by 17. Find the sum of all 0 to 9 pandigital numbers with this property.
 """
 from itertools import permutations
 
