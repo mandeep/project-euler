aac3c270ee9b0fc9bc87dd5fc6b5b5a377aac87c
Mandeep Bhutani <bhutanimandeep@gmail.com>
2016-01-21 20:45:08 -0600

    Performance improvement

--- Problem 39 - Integer Right Triangles.py
+++ Problem 39 - Integer Right Triangles.py
@@ -8,16 +8,15 @@ sides, {a, b, c}, there are exactly three solutions for p = 120:
 {20, 48, 52}, {24, 45, 51}, and {30, 40, 50}.
 For which value of p <= 1000, is the number of solutions maximised?
 """
-
 result = []
-for x in range(1, 1000+1):
+for p in range(1, 1000+1):
     total = []
-    for a in range(1, x):
-        for b in range(1, x):
-            c = x - a - b
+    for a in range(1, p):
+        for b in range(1, p):
+            c = p - a - b
             if a ** 2 + b ** 2 == c ** 2:
                 if sorted((a, b, c)) not in total:
                     total.append(sorted((a, b, c)))
     if len(total) > 3:
-        result.append((len(total), x))
+        result.append((len(total), p))
 print(max(result))
