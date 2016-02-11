c827a858a415dbe03dc12608d8c7299bf6867fa7
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-11 11:33:25 -0600

    Optimized performance: Removed guesstimate range and created function to save on print resources

--- Problem 9 - Special Pythagorean Triplet.py
+++ Problem 9 - Special Pythagorean Triplet.py
@@ -10,9 +10,12 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 + 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product of a*b*c.
 """
-for a in range(1, 500):
-    for b in range(1, 500):
-        c = 1000 - a - b
-        if a ** 2 + b ** 2 == c ** 2:
-            print(a * b * c)
-            break
+
+
+def pythagorean_triplet(p):
+    for a in range(1, p+1):
+        for b in range(1, p+1 - a):
+            c = p - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                return a * b * c
+print(pythagorean_triplet((1000)))
