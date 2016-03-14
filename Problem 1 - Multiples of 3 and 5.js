c017183bf9bd4bc1f914fd98ddf9c2fe1e85472d
mandeepbhutani <bhutanimandeep@gmail.com>
2016-03-13 19:45:19 -0500

    Added absolute operator

--- Problem 1 - Multiples of 3 and 5.js
+++ Problem 1 - Multiples of 3 and 5.js
@@ -12,7 +12,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 function sumAll(n) {
     var result = 0;
     for (var i = 0; i < 1000; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
+        if (i % 3 === 0 || i % 5 === 0) {
             result += i;
         }
     }
