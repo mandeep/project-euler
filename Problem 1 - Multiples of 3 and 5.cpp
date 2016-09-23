d037229851abc130736db389d1eb9936019d1303
mandeepbhutani <bhutanimandeep@gmail.com>
2016-09-23 16:12:11 -0500

    Switched to list initialization for sum variable

--- Problem 1 - Multiples of 3 and 5.cpp
+++ Problem 1 - Multiples of 3 and 5.cpp
@@ -12,7 +12,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 #include <iostream>
 
 int main() {
-    int sum = 0;
+    int sum {0};
     for (int i = 0; i < 1000; ++i) {
         if ( i % 3 == 0 || i % 5 == 0) {
             sum += i;
