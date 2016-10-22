6e32d674028072b7f70b90f483361effc6511faa
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-22 13:10:03 -0500

    Refactored problem using filter and fold

--- Problem_1_Multiples_of_3_and_5.rs
+++ Problem_1_Multiples_of_3_and_5.rs
@@ -1,20 +1,18 @@
-//
-// Project: Project Euler - Problem 1: Multiples of 3 and 5
-// Author: Mandeep Bhutani
-// Date: 10/20/2016
-//
-// Problem:
-// If we list all the natural numbers below 10 that are multiples of
-// 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-// Find the sum of all the multiples of 3 or 5 below 1000.
-//
+/*
+Project: Project Euler - Problem 1: Multiples of 3 and 5
+Author: Mandeep Bhutani
+Date: 10/20/2016
+
+Problem:
+If we list all the natural numbers below 10 that are multiples of
+3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 
 fn main() {
-    let mut summation = 0;
-    for number in 1..1000 {
-        if number % 3 == 0 || number % 5 == 0 {
-            summation += number
-        }
-    }
+    let summation: u64 = (1..1000)
+        .filter(|&x| x % 3 == 0 || x % 5 == 0)
+        .fold(0, |summation, x| summation + x);
     println!("{}", summation);
 }
