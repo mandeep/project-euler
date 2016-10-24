a8469ddf8aa52e7ef867954f0f59ace27175f1cb
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-23 21:04:18 -0500

    Solved problem 5 with Rust

---
+++ Problem_5_Smallest_Multiple.rs
@@ -0,0 +1,19 @@
+/*
+Title: Project Euler - Problem 5: Smallest Multiple
+Author: Mandeep Bhutani
+Date: 10/23/2016
+
+Problem: 2520 is the smallest number that can be divided by each
+of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible
+by all of the numbers from 1 to 20?
+*/
+extern crate num;
+
+use num::integer::lcm;
+
+fn main() {
+    let divisors: Vec<i32> = (1..20+1).collect();
+    let num = divisors.iter().fold(1, |a, &b| lcm(a, b));
+    println!("{}", &num);
+}
