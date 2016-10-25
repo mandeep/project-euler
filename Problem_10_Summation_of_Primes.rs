cfb22ec60d7fa4f046df16784c25a77286fec5b6
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-24 21:52:23 -0500

    Refactored u32 to u64 and replaced fold() with sum()

--- Problem_10_Summation_of_Primes.rs
+++ Problem_10_Summation_of_Primes.rs
@@ -6,15 +6,15 @@ Date: 10/22/2016
 Problem: The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
-fn is_prime(n: u32) -> bool {
+fn is_prime(n: u64) -> bool {
     if n < 2 { return false; }
     let temp = n as f64;
-    let limit = temp.sqrt() as u32;
+    let limit = temp.sqrt() as u64;
     for i in 2..limit+1 { if n % i == 0 { return false; } }
     true
 }
 
 fn main() {
-    let sum: u64 = (1..2000000).filter(|&x| is_prime(x)).fold(0, |sum, x| sum + x as u64);
+    let sum: u64 = (1..2000000).filter(|&x| is_prime(x)).sum();
     println!("{:?}", sum);
 }
