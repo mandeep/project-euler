1854752f4471019bed2d39387660b8f8eb62d2bf
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-26 01:01:16 -0500

    Removed temp variable from is_prime

--- Problem_7_10001st_Prime.rs
+++ Problem_7_10001st_Prime.rs
@@ -6,16 +6,15 @@ Date: 10/21/2016
 Problem: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
 we can see that the 6th prime is 13. What is the 10,001st prime number?
 */
-fn is_prime(n: i32) -> bool {
+fn is_prime(n: u32) -> bool {
     if n < 2 { return false; }
-    let temp = n as f64;
-    let limit: i32 = temp.sqrt() as i32;
+    let limit: u32 = (n as f32).sqrt() as u32;
     for i in 2..limit+1 { if n % i == 0 { return false; } }
     true
 }
 
 fn main() {
-    let number: Vec<i32> = (1..)
+    let number: Vec<u32> = (1..)
         .filter(|&x| is_prime(x))
         .take(10001)
         .collect();
