93d3a5ef5a9f1a2b33782d861a2c46ea763d2018
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-25 21:59:54 -0500

    Solved problem 20 with Rust

---
+++ Problem_20_Factorial_Digit_Sum.rs
@@ -0,0 +1,38 @@
+/*
+Project: Project Euler - Problem 20: Factorial Digit Sum
+Author: Mandeep Bhutani
+Date: 10/25/2016
+
+Problem: n! means n * (n-1) * ... * 3 * 2 * 1
+For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Find the sum of the digits in the number 100!
+*/
+extern crate num;
+
+use num::Integer;
+use num::BigUint;
+use num::traits::{One, ToPrimitive};
+
+
+fn factorial(n: usize) -> BigUint {
+    (1..n+1).fold(One::one(), |sum, x| sum * BigUint::from(x))
+}
+
+
+fn sum_digits(n: BigUint) -> u32 {
+    let mut num: BigUint = n;
+    let mut digits: Vec<u32> = Vec::new();
+    for _ in 1..250 {
+        let digit = Integer::mod_floor(&num, &BigUint::from(10 as u32));
+        num = Integer::div_floor(&num, &BigUint::from(10 as u32));
+        digits.push(ToPrimitive::to_u32(&digit).unwrap());
+    }
+    let summation: u32 = digits.iter().sum();
+    summation
+}
+
+
+fn main() {
+    println!("{}", sum_digits(factorial(100)));
+}
