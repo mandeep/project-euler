ff9294fdc0340e07cb6acad1df89b0c87a935cae
mandeep <mandeep@users.noreply.github.com>
2016-12-24 22:37:29 -0600

    Rewrote maximal_digit_sum to use max() and digit_sum to use genexp

--- Problem_56_Powerful_Digit_Sum.py
+++ Problem_56_Powerful_Digit_Sum.py
@@ -8,9 +8,10 @@ zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros.
 Despite their size, the sum of the digits in each number is only 1. Considering
 natural numbers of the form, a^b, where a, b < 100, what is their maximum digital sum?
 """
-maximal_digit_sum = set()
+maximal_digit_sum = 0
 for a in range(1, 100):
     for b in range(1, 100):
-        digit_sum = sum([int(c) for c in str(a ** b)])
-        maximal_digit_sum.add(digit_sum)
-print(max(maximal_digit_sum))
+        digit_sum = sum(int(c) for c in str(a ** b))
+        maximal_digit_sum = max(maximal_digit_sum, digit_sum)
+
+print(maximal_digit_sum)
