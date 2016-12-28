e89f71483153822cb5c041960ccab42761a2c806
mandeep <mandeep@users.noreply.github.com>
2016-12-28 02:17:36 -0600

    Replaced list comprehensions with generator expressions

--- Problem_6_Sum_Square_Difference.py
+++ Problem_6_Sum_Square_Difference.py
@@ -15,6 +15,7 @@ numbers and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred
 natural numbers and the square of the sum.
 """
-sum_of_squares = sum([x ** 2 for x in range(1, 100+1)])
-square_of_sums = sum([x for x in range(1, 100+1)]) ** 2
+sum_of_squares = sum(x ** 2 for x in range(1, 100+1))
+square_of_sums = sum(x for x in range(1, 100+1)) ** 2
+
 print(square_of_sums - sum_of_squares)
