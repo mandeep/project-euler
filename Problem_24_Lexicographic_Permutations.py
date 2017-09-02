8c304392c54be8aa2ca07d0c789f79301faa6973
mandeep <mandeep@users.noreply.github.com>
2017-09-01 19:33:54 -0500

    Replace single letter variable name with a more descriptive name

--- Problem_24_Lexicographic_Permutations.py
+++ Problem_24_Lexicographic_Permutations.py
@@ -14,7 +14,7 @@ of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 from itertools import permutations
 
 count = 0
-for x in permutations("0123456789"):
+for number in permutations("0123456789"):
     count += 1
     if count == 1000000:
-        print("".join(x))
+        print("".join(number))
