e0434a26ea57bae12df1bd9d062f8dae0d8fb5bd
mandeep <mandeep@users.noreply.github.com>
2017-09-18 09:32:12 -0500

    Refactor to use function that factorials each digit to decrease the runtime by 50%

--- Problem_34_Digit_Factorials.py
+++ Problem_34_Digit_Factorials.py
@@ -7,13 +7,23 @@ Problem: 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 Find the sum of all numbers which are equal to the sum of the factorial of
 their digits. Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
-Analysis: Factorial(9) is used as the upper bound 
+Analysis: Factorial(9) is used as the upper bound
 """
 from math import factorial
 
-result = []
+
+def factorial_digits(n):
+    summation = 0
+    while n > 0:
+        summation += factorial(n % 10)
+        n //= 10
+    return summation
+
+
+result = 0
 for number in range(1, factorial(9)):
-    total = [factorial(int(digit)) for digit in str(number)]
-    if sum(total) == number and sum(total) > 2:
-        result.append(number)
-print(sum(result))
+    total = factorial_digits(number)
+    if total == number and total > 2:
+        result += number
+
+print(result)
