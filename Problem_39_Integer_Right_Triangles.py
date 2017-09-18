ee5017fb0d9ea5bc006168afc6f8dcb54bdfac04
mandeep <mandeep@users.noreply.github.com>
2017-09-18 13:20:54 -0500

    Optimize nested for loop for 75% increase in speed

--- Problem_39_Integer_Right_Triangles.py
+++ Problem_39_Integer_Right_Triangles.py
@@ -8,15 +8,16 @@ sides, {a, b, c}, there are exactly three solutions for p = 120:
 {20, 48, 52}, {24, 45, 51}, and {30, 40, 50}.
 For which value of p <= 1000, is the number of solutions maximised?
 """
-result = []
+maximum = (0, 0)
+
 for p in range(1, 1000+1):
-    total = []
+    solutions = set()
     for a in range(1, p):
-        for b in range(1, p):
+        for b in range(a, p - a):
             c = p - a - b
             if a ** 2 + b ** 2 == c ** 2:
-                if sorted((a, b, c)) not in total:
-                    total.append(sorted((a, b, c)))
-    if len(total) > 3:
-        result.append((len(total), p))
-print(max(result))
+                solutions.add((a, b, c))
+    if len(solutions) > 3 and (len(solutions), p) > maximum:
+        maximum = (len(solutions), p)
+
+print(maximum)
