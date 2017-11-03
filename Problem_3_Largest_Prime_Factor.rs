4ae990abb79bc09a9cedc1c0bfbdcd0f4537cd84
mandeep <mandeep@users.noreply.github.com>
2017-11-03 09:24:22 -0500

    Refactor problem 3 to use less naive solution

--- Problem_3_Largest_Prime_Factor.rs
+++ Problem_3_Largest_Prime_Factor.rs
@@ -1,17 +1,11 @@
-fn is_prime(n: u64) -> bool {
-    if n < 2 { return false; }
-    let limit: u64 = (n as f64).sqrt() as u64;
-    for i in 2..limit+1 { if n % i == 0 { return false; } }
-    true
-}
-
 fn main() {
-    let number: u64 = 600851475143;
-    let limit = (number as f64).sqrt() as u64;
-    let factors = (2..limit+1)
-        .filter(|&i| number % i == 0);
-    let prime_factors: Vec<u64> = factors
-        .filter(|&j| is_prime(j))
-        .collect();
-    println!("{:?}", prime_factors.last().unwrap());
+    let mut i = 2;
+    let mut number: u64 = 600851475143;
+    while i * i < number {
+        while number % i == 0 {
+            number /= i;
+        }
+        i += 1;
+    }
+    println!("{}", number);
 }
