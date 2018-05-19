31d3779276c4e2fe617ca034818b0b5d1e7dc7e7
mandeep <mandeep@users.noreply.github.com>
2018-05-19 01:42:05 -0500

    Move largest palindromic code into separate function

--- problem_4_largest_palindrome_product.py
+++ problem_4_largest_palindrome_product.py
@@ -12,10 +12,14 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 def is_palindrome(something):
     return str(something) == str(something)[::-1]
 
-result = 1
-for x in range(100, 1000):
-    for y in range(x, 1000):
-        if is_palindrome(x * y) and (x * y) > result:
-            result = (x * y)
 
-print(result)
+def largest_palindrome_product(number):
+    product = 1
+    for x in range(100, number):
+        for y in range(x, number):
+            if is_palindrome(x * y) and (x * y) > product:
+                product = (x * y)
+    return product
+
+
+print(largest_palindrome_product(1000))
