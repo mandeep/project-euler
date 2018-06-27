a79b88170ce91bffff6bea0df5c913f02e3be793
mandeep <mandeep@users.noreply.github.com>
2018-06-26 23:08:10 -0700

    Use Euclid's formula to find pythagorean triplet

--- problem_9_special_pythagorean_triplet.py
+++ problem_9_special_pythagorean_triplet.py
@@ -13,11 +13,19 @@ Find the product of a*b*c.
 
 
 def pythagorean_triplet(p):
-    for a in range(1, p+1):
-        for b in range(a, p+1 - a):
-            c = p - a - b
-            if a ** 2 + b ** 2 == c ** 2:
-                return a * b * c
+    """Find the pythagorean triplet using Euclid's formula.
+
+    Euclid's formula states that any pythagorean triplet (a, b, c)
+    can be written as a = m^2 - n^2, b = 2mn, c = m^2 + n^2.
+    Since a + b + c = 1000 we can substitute the variables in
+    Euclid's formula for a, b, and c to obtain:
+    m^2 - n^2 + 2mn + m^2 + n^2 = 1000. Simplifying we obtain:
+    2m^2 + 2mn = 1000 and then m(m + n) = 500.
+    """
+    for n in range(1, 500):
+        for m in range(n + 1, 500 - n):
+            if m ** 2 + m * n == 500:
+                return (m ** 2 - n ** 2) * (2 * m * n) * (m ** 2 + n ** 2)
 
 
 print(pythagorean_triplet((1000)))
