b465595c5d70cbbf2c4410d7c78d3aac65445299
mandeep <mandeep@users.noreply.github.com>
2018-06-28 18:55:26 -0700

    Revert "Remove Rust solutions"
    This reverts commit e1826efce06c733cccab6764a5d3ea764173d713.

---
+++ Problem_6_Sum_Square_Difference.rs
@@ -0,0 +1,29 @@
+/*
+Project: Project Euler - Problem 6: Sum Square Difference
+Author: Mandeep Bhutani
+Date: 10/21/2016
+
+Problem:
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 - 385 = 2640.
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+*/
+fn sum_squares(n: u64) -> u64 {
+    n * (n + 1) * (2 * n + 1) / 6
+}
+
+fn square_sum(n: u64) -> u64 {
+    (n * (n + 1) / 2).pow(2)
+}
+
+fn main () {
+    let difference = square_sum(100) - sum_squares(100);
+    println!("{}", difference);
+}
