ffa077677d266e35729d6a3c00b1edf467e4a752
mandeep <mandeep@users.noreply.github.com>
2018-06-30 10:41:41 -0700

    Fix inequality and compute integer length

--- problem_25_1000-digit_fibonacci_number.py
+++ problem_25_1000-digit_fibonacci_number.py
@@ -8,10 +8,18 @@ Fn = Fn-1 + Fn-2 where F1 = 0 and F2= 1. The 12th term, F12 is the first term
 to contain three digits. What is the first term in the Fibonacci sequence to
 contain 1000 digits?
 """
-term = 0
-a, b = 0, 1
+import math
 
-while len(str(a)) <= 1000:
-    a, b = b, a + b
-    term += 1
-print(term)
+
+def n_digit_fibonacci(n):
+    term = 1
+    a, b = 1, 1
+
+    while math.floor(math.log10(a)) + 1 < n:
+        a, b = b, a + b
+        term += 1
+
+    return term
+
+
+print(n_digit_fibonacci(1000))
