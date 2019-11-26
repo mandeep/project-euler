49d6a3d69f5e87f3c5f1126bf0b2ecfc3cf93f21
mandeep <mandeep@users.noreply.github.com>
2019-11-26 16:59:43 -0500

    Move logic inside function

--- problem_17_number_letter_counts.py
+++ problem_17_number_letter_counts.py
@@ -13,34 +13,40 @@ dictionary. If the value is not defined, subtraction takes places until the
 value is determined. For example, 102 is added as one hundred then as two to
 define "one hundred two".
 """
-elements = ((1, "one"), (2, "two"), (3, "three"), (4, "four"), (5, "five"),
-            (6, "six"), (7, "seven"), (8, "eight"), (9, "nine"), (10, "ten"),
-            (11, "eleven"), (12, "twelve"), (13, "thirteen"), (14, "fourteen"),
-            (15, "fifteen"), (16, "sixteen"), (17, "seventeen"),
-            (18, "eighteen"), (19, "nineteen"), (20, "twenty"), (30, "thirty"),
-            (40, "forty"), (50, "fifty"), (60, "sixty"), (70, "seventy"),
-            (80, "eighty"), (90, "ninety"), (100, "one hundred"),
-            (200, "two hundred"), (300, "three hundred"), (400, "four hundred"),
-            (500, "five hundred"), (600, "six hundred"), (700, "seven hundred"),
-            (800, "eight hundred"), (900, "nine hundred"), (1000, "one thousand"))
 
-combination = ""
-for i in range(1, 1001):
-    for number, word in elements[::-1]:
-        while i >= number:
-            combination += word
-            i -= number
 
-combination = combination.replace(" ", "")
+def number_letter_counts(n):
+    elements = ((1, "one"), (2, "two"), (3, "three"), (4, "four"), (5, "five"),
+                (6, "six"), (7, "seven"), (8, "eight"), (9, "nine"), (10, "ten"),
+                (11, "eleven"), (12, "twelve"), (13, "thirteen"), (14, "fourteen"),
+                (15, "fifteen"), (16, "sixteen"), (17, "seventeen"),
+                (18, "eighteen"), (19, "nineteen"), (20, "twenty"), (30, "thirty"),
+                (40, "forty"), (50, "fifty"), (60, "sixty"), (70, "seventy"),
+                (80, "eighty"), (90, "ninety"), (100, "one hundred"),
+                (200, "two hundred"), (300, "three hundred"), (400, "four hundred"),
+                (500, "five hundred"), (600, "six hundred"), (700, "seven hundred"),
+                (800, "eight hundred"), (900, "nine hundred"), (1000, "one thousand"))
 
-result = len(combination)
+    combination = ""
+    for i in range(1, n+1):
+        for number, word in elements[::-1]:
+            while i >= number:
+                combination += word
+                i -= number
 
-# Because the problem uses British English, the word and needs to be added.
-# One hundred sixty two in American English, a.k.a., best English, needs to
-# be one hundred and sixty two.
-for british in range(100, 1000):
-    result += 3
+    combination = combination.replace(" ", "")
 
-# There are nine values between one hundred and one thousand that don't use
-# the word "and". They are one hundred, two hundred, three hundred,...
-print(result - (9 * 3))
+    american_count = len(combination)
+
+    # Because the problem uses British English, the word and needs to be added.
+    # One hundred sixty two in American English, a.k.a., best English, needs to
+    # be one hundred and sixty two.
+    for british in range(100, n):
+        american_count += 3
+
+    # There are nine values between one hundred and one thousand that don't use
+    # the word "and". They are one hundred, two hundred, three hundred,...
+    return american_count - (9 * 3)
+
+
+print(number_letter_counts(1000))
