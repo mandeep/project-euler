73f0915bbc632af3463a73bf78f317a9ebb175d1
mandeep <mandeep@users.noreply.github.com>
2019-11-26 20:17:43 -0500

    Move logic inside function

--- problem_24_lexicographic_permutations.py
+++ problem_24_lexicographic_permutations.py
@@ -13,8 +13,12 @@ of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 """
 from itertools import permutations
 
-count = 0
-for number in permutations("0123456789"):
-    count += 1
-    if count == 1000000:
-        print("".join(number))
+
+def nth_lexicographic_permutation(n):
+    count = 0
+    for number in permutations("0123456789"):
+        count += 1
+        if count == n:
+            return "".join(number)
+
+print(nth_lexicographic_permutation(1000000))
