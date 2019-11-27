fde59117657408ddbd82b27205c144931f033c8e
mandeep <mandeep@users.noreply.github.com>
2019-11-26 20:26:19 -0500

    Refactor logic to use maximum and length variables instead of tuples in a list

--- problem_26_reciprocal_cycles.py
+++ problem_26_reciprocal_cycles.py
@@ -4,7 +4,7 @@ Author: Mandeep Bhutani
 Date: 4/15/2015
 
 Problem: A unit fraction contains 1 in the numerator. Where 0.1(6) means 0.166666...,
-and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle. 
+and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
 its decimal fraction part.
 
@@ -15,12 +15,20 @@ due to the find function's error exception.
 """
 from decimal import getcontext, Decimal
 
+
 getcontext().prec = 3000
 
-total = [str(Decimal(1) / Decimal(x))[2:] for x in range(1, 1000)]
-result = []
-for number in total:
-    cycle_length = (number * 2).find(number[:2000], 1, -1)
-    if cycle_length < 3000 and cycle_length != -1:
-        result.append((cycle_length, total.index(number) + 1))
-print(max(result))
+
+def find_recurring_cycle(n):
+    decimals = [str(Decimal(1) / Decimal(x))[2:] for x in range(1, n)]
+    cycle_length = 0
+    maximum_cycle = 0
+    for digit in decimals:
+        length = (digit * 2).find(digit[:n*2], 1, -1)
+        if length < getcontext().prec and length > cycle_length:
+            cycle_length = length
+            maximum_cycle = decimals.index(digit) + 1
+    return maximum_cycle
+
+
+print(find_recurring_cycle(1000))
